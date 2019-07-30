42fea1601369a12b2b8add5ab4c81f7636552281
Paul Kravchenko <pk@sitecore.net>
2019-07-30 12:53:09 +0300

    Fixed getRenderingCaching pipeline to populate GetRenderingCachingArgs.CachingDefinition with results

---
+++ src/Sitecore.Support.309807/XA/Foundation/PlaceholderSettings/Pipelines/GetRenderingCaching/GetPlaceholderRenderingCaching.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Data;
+using Sitecore.Mvc.Extensions;
+using Sitecore.Mvc.Presentation;
+using Sitecore.XA.Foundation.Presentation.HtmlCaching;
+using Sitecore.XA.Foundation.Presentation.Pipelines.GetRenderingCaching;
+using Sitecore.XA.Foundation.Presentation.Services;
+
+namespace Sitecore.Support.XA.Foundation.PlaceholderSettings.Pipelines.GetRenderingCaching
+{
+  public class GetPlaceholderRenderingCaching
+  {
+    private readonly IPlaceholderCachingResolver _placeholderCachingResolver;
+
+    public GetPlaceholderRenderingCaching(IPlaceholderCachingResolver placeholderCachingResolver)
+    {
+      _placeholderCachingResolver = placeholderCachingResolver;
+    }
+
+    public void Process(GetRenderingCachingArgs args)
+    {
+      if (args.Rendering?.Item?.Database != null)
+      {
+        CachingOptions cachingOptionsFromContext = _placeholderCachingResolver.GetCachingOptionsFromContext(PageContext.Current, ID.Parse(args.Rendering.DeviceId), args.Rendering.Placeholder);
+        if (cachingOptionsFromContext != null && cachingOptionsFromContext.ResetCachingOptions)
+        {
+          args.Rendering["Cacheable"] = cachingOptionsFromContext.Cacheable.ToBoolString();
+          args.Rendering["Cache_VaryByData"] = cachingOptionsFromContext.VaryByData.ToBoolString();
+          args.Rendering["Cache_VaryByDevice"] = cachingOptionsFromContext.VaryByDevice.ToBoolString();
+          args.Rendering["Cache_VaryByLogin"] = cachingOptionsFromContext.VaryByLogin.ToBoolString();
+          args.Rendering["Cache_VaryByParameters"] = cachingOptionsFromContext.VaryByParm.ToBoolString();
+          args.Rendering["Cache_VaryByQueryString"] = cachingOptionsFromContext.VaryByQueryString.ToBoolString();
+          args.Rendering["Cache_VaryByUser"] = cachingOptionsFromContext.VaryByUser.ToBoolString();
+          args.Rendering["InheritedCaching"] = "1";
+          args.CachingDefinition = args.Rendering.Caching;
+          args.AbortPipeline();
+        }
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.309807/XA/Foundation/Presentation/Pipelines/GetRenderingCaching/GetSiteLevelCaching.cs
@@ -0,0 +1,48 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Layouts;
+using Sitecore.Mvc.Presentation;
+using Sitecore.XA.Foundation.Presentation;
+using Sitecore.XA.Foundation.Presentation.Extensions;
+using Sitecore.XA.Foundation.Presentation.Pipelines.GetRenderingCaching;
+using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+
+namespace Sitecore.Support.XA.Foundation.Presentation.Pipelines.GetRenderingCaching
+{
+  public class GetSiteLevelCaсhing
+  {
+    private readonly IPresentationContext _presentationContext;
+
+    public GetSiteLevelCaсhing(IPresentationContext presentationContext)
+    {
+      _presentationContext = presentationContext;
+    }
+
+    public void Process(GetRenderingCachingArgs args)
+    {
+      Item item = _presentationContext.PresentationItem?.FirstChildInheritingFrom(Templates.CacheSettingsFolder.ID);
+      if (item != null)
+      {
+        string value = args.Rendering.RenderingItem?.ID.ToString() ?? args.Rendering.RenderingItemPath;
+        foreach (Item child in item.Children)
+        {
+          if (child.InheritsFrom(Templates.CacheSettings.ID) && child[Templates.CacheSettings.Fields.Renderings].Contains(value))
+          {
+            PopulateCacheOptions(args.Rendering, child);
+            args.Rendering["InheritedCaching"] = "1";
+            args.CachingDefinition = args.Rendering.Caching;
+            break;
+          }
+        }
+      }
+    }
+
+    protected virtual void PopulateCacheOptions(Rendering rendering, Item cacheSettingsItem)
+    {
+      if (cacheSettingsItem.InheritsFrom(Templates.Caching.ID))
+      {
+        RenderingCaching renderingCaching = RenderingCaching.Parse(cacheSettingsItem);
+        rendering.SetCachingOptions(renderingCaching);
+      }
+    }
+  }
+}
